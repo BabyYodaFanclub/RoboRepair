3cd17c5deacb384503f85bb3b3a48e243fbe8f26
Luca Nixdorf <luca@familie-nixdorf.de>
2020-02-01 14:30:41 +0100

    parsing for line params added

--- BotProject/DummyLevel.py
+++ BotProject/DummyLevel.py
@@ -1,5 +1,3 @@
-from datetime import timedelta
-
 from telegram import ChatAction
 
 from BotBase import BotBase
@@ -12,12 +10,16 @@ class DummyLevel(LevelBase):
     def __init__(self):
         super().__init__()
 
+    def accept_chat_start(self, bot: BotBase, chat_id: str, voice_message, global_state: State) -> 'LevelBase':
+        bot.send_text(chat_id, 'TEST')
+        return self
+
     def accept_text_message(self, bot: BotBase, chat_id: str, text: str, global_state: State) -> 'LevelBase':
         bot.send_chat_action(chat_id, ChatAction.TYPING)
-        bot.schedule_message(chat_id, f'Hallo {global_state.name}!', timedelta(seconds=5))
-        global_state.name = 'nameee'
+        # bot.delayed_type_message(chat_id, f'Hallo!\n{text}')
+        # bot.send_iteratively_edited_message(chat_id, ['Hallo', 'Hola', 'Hiho','Hallo', 'Hola', 'Hiho','Hallo', 'Hola', 'Hiho','Hallo', 'Hola', 'Hiho'])
 
         return self
 
     def accept_voice_message(self, bot: BotBase, chat_id: str, voice_message, global_state: State) -> 'LevelBase':
-        pass
+        return self
