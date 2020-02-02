ba4475ed687f8ae28d50da4aba517cd36cee1838
Kopogtias VH <xtremefeel@gmx.de>
2020-02-02 14:44:30 +0100

    okokok

--- BotProject/Levels/L1_Level1/Level1.py
+++ BotProject/Levels/L1_Level1/Level1.py
@@ -14,16 +14,20 @@ class Level1(AbstractLevel):
         self.set_message_sequence('00_start', self.end_dialog_1)
 
         self.valid_keys = {
-            "how can i help": lambda *x: self.set_message_sequence("help"),
-            "what is your model?": lambda *x: self.set_message_sequence("model"),
-            "which model are you?": lambda *x: self.set_message_sequence("model"),
-            "what is your model": lambda *x: self.set_message_sequence("model"),
-            "which model are you": lambda *x: self.set_message_sequence("model"),
+            "how can i help": lambda b, s: self.run_message_sequence(b, s, "help"),
+            "what is your model?": lambda b, s: self.run_message_sequence(b, s, "model"),
+            "which model are you?": lambda b, s: self.run_message_sequence(b, s, "model"),
+            "what is your model": lambda b, s: self.run_message_sequence(b, s, "model"),
+            "which model are you": lambda b, s: self.run_message_sequence(b, s, "model"),
             "show me what you see": self.send_visuals,
             "what do you see?": self.send_visuals,
             "what do you see": self.send_visuals,
-            "hokus pokus camerus restartikus": lambda *x: self.set_message_sequence("camrestart", self.end_camera_restart_dialog())
+            "hokus pokus camerus restartikus": lambda b, s: self.run_message_sequence(b, s, "camrestart", self.end_camera_restart_dialog)
         }
+
+    def run_message_sequence(self, bot, global_state, name: str, callback: callable = None):
+        self.set_message_sequence(name, callback)
+        self.start_message_sequence(bot, global_state)
 
     def send_visuals(self, bot: BotBase, global_state: State):
         if not self.camera_is_on:
@@ -31,23 +35,29 @@ class Level1(AbstractLevel):
             SendPictureAction('', 0, [self.current_dir() + "/01black.png"]).send(bot, global_state, lambda *x: None)
                                                                )
         else:
-            ImmediateNextAction("I can see this:", 0, []).send(bot, global_state, lambda *x:
-            SendPictureAction('', 0, [self.current_dir() + "everything good.png"]).send(bot, global_state, lambda *x: None)
-                                                               )
+            ImmediateNextAction.static_send(bot,
+                                            global_state,
+                                            "I can see this:",
+                                            lambda *x:
+                SendPictureAction('', 0, [self.current_dir() + "everything good.png"]).send(bot, global_state, lambda *x: None)
+            )
 
     def get_file(self):
         return __file__
 
     def level_resume(self, bot: BotBase, global_state: State, chat_type: ChatType, message) -> 'LevelBase':
         if chat_type is ChatType.VOICE:
-            ImmediateNextAction("", 0, []).send_error(bot, global_state, lambda *x: None)
+            ImmediateNextAction.static_send_error(bot, global_state)
             return
 
         message = message.lower()
+
         for key, val in self.valid_keys.items():
-            if message in key:
+            if message == key or key in message:
                 val(bot, global_state)
-        ImmediateNextAction("", 0, []).send_error(bot, global_state, lambda *x: None)
+                return
+
+        ImmediateNextAction.static_send_error(bot, global_state)
 
 
     def end(self, global_state: State) -> 'LevelBase':
